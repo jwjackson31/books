316ecfb15d169848d350c6d9a9551541625bf82f
Marlon Jackson <jwjackson31@gmail.com>
2026-01-25 20:20:52 -0700

    Added code to Booklist and BookShow to have the titles appear after entered -Learned about updating objects without mutating State

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate'
-
+import BookList from './components/BookList'
 
 function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
+        const updatedBooks = [
+            ...books,
+            {
+                id: Math.round(Math.random() * 9999),
+                title,
+            },
+        ];
+        setBooks(updatedBooks);
         console.log('Need to add book with:', title)
+
     };
 
-    return <div>
-        <BookCreate onCreate={createBook} />
-    </div>
+
+
+    return (
+        <div className="app">
+            <BookList books={books} />
+            <BookCreate onCreate={createBook} />
+        </div>
+    )
 }
 
 export default App;
